898e799dcefe1706696c05a86c75da92fa4217ae
Yusuf Olarewaju <kolarewaju9@gmail.com>
2026-06-01 18:59:49 +0100

    Tried working with logistic regression, make some code but not perfected yet.

--- notebooks/notebook3.py
+++ notebooks/notebook3.py
@@ -82,3 +82,52 @@ mse = np.mean((Y_test - predictions) ** 2)
 print("Regression model trained from scratch")
 print(f"-> Calculated the coefficient: {best_fit_theta}")
 print(f"-> Mean Square Error: {mse:.2f}")
+
+
+# logit beginning
+print("This is the beginning of Logistic Regression")
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+X_train_b = np.c_[np.ones((len(X_train), 1)), X_train]
+X_test_b = np.c_[np.ones((len(X_test), 1)), X_test]
+
+
+# setting hyperparameters for gradient descent
+learning_rate = 0.01
+iterations = 1000
+m = len(Y_train)
+
+# Initializing the coefficient to zero
+theta = np.zeros(X_train_b.shape[1])
+
+# Loop to upgrade coefficients gradually
+for i in range(iterations):
+    # calculating the linear steps, then applying the segmoid function
+    linear_model = np.dot(X_train_b, theta)
+    predictions = sigmoid(linear_model)
+    # calculating the gradient
+    # (1/m) * X^T * (predictions - actual). the formula calculating the gradient descent
+    gradient = np.dot(X_train_b.T, (predictions - Y_train)) / m
+
+    # we have to update the coefficient by taking a step in the opposite direction of the gradinet
+    # theta -= learning_rate * gradient
+
+
+# get the probability for the test
+test_linear_model = np.dot(X_test_b, theta)
+test_probabilities = sigmoid(test_linear_model)
+
+# convert probabilities to strict 0 and 1 predictions
+final_prediction = [1 if prob >= 0.5 else 0 for prob in test_probabilities]
+
+# checking for the acuracy using Accuracy(percentage correct)
+correct_prediction = np.sum(final_prediction == Y_test)
+accuracy = correct_prediction / len(Y_test)
+
+print("Logit regression model trained via Gradient descent.")
+print(f"-> Calculated Coefficients {theta}")
+print(f"-> Model Accuracy {accuracy * 100:.3f}%")
