553f9b419fc4a8fb06ddd72d704c33e873e3cf30
Yusuf Olarewaju <kolarewaju9@gmail.com>
2026-06-02 16:46:08 +0100

    train_logit.py folder was created for the logistics regression model.

---
+++ src/modeling/train_logit.py
@@ -0,0 +1,50 @@
+import numpy as np
+from src.data_processing import data_cleaning
+
+
+def sigmoid(z):
+    z = np.clip(z, -500, 500)
+    return 1 / (1 + np.exp(-z))
+
+
+def main():
+    X_train_scaled, X_test_scaled, Y_train, Y_test = data_cleaning()
+
+    Y_train = Y_train.reshape(-1, 1)
+    Y_test = Y_test.reshape(-1, 1)
+
+    X_train_b = np.c_[np.ones((len(X_train), 1)), X_train]
+    X_test_b = np.c_[np.ones((len(X_test), 1)), X_test]
+
+    # setting hyperparameters for gradient descent
+    learning_rate = 0.001
+    iterations = 5000
+    Y_train = Y_train.reshape(-1)
+    m = len(Y_train)
+
+    # Loop to upgrade coefficients gradually
+    for i in range(iterations):
+        # calculating the linear steps, then applying the segmoid function
+        linear_model = np.dot(X_train_b, theta)
+        predictions = sigmoid(linear_model)
+        # calculating the gradient
+        # (1/m) * X^T * (predictions - actual). the formula calculating the gradient descent
+        gradient = np.dot(X_train_b.T, (predictions - Y_train)) / m
+
+        # we have to update the coefficient by taking a step in the opposite direction of the gradinet
+        theta -= learning_rate * gradient
+        # get the probability for the test
+    test_linear_model = np.dot(X_test_b, theta)
+    test_probabilities = sigmoid(test_linear_model)
+
+    # convert probabilities to strict 0 and 1 predictions
+    final_prediction = (test_probabilities >= 0.5).astype(int)
+
+    # checking for the acuracy using Accuracy(percentage correct)
+    accuracy = np.mean(final_prediction == Y_test)
+    print(f"-> Calculated Coefficients {theta}")
+    print(f"-> Model Accuracy {accuracy * 100:.3f}%")
+
+
+if __name__ == "__main__":
+    main()
